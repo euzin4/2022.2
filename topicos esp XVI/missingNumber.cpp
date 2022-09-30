c7ba23e8deca0b0f887af1ec1adc84d01ccae98c
kevin willian <kevin_willian@hotmail.com.br>
2022-09-29 21:40:15 -0300

    Create missingNumber.cpp

---
+++ topicos esp XVI/missingNumber.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> v;
+int n,x;
+
+int main()
+{
+    cin >> n;
+    for(int i=0;i<n-1;i++){
+        cin >> x;
+        v.push_back(x);
+    }
+}
