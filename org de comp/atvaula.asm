9b7e95105f2b2eb1cf11190c2a4ee229e1cda379
kevin willian <kevin_willian@hotmail.com.br>
2022-10-17 02:01:17 -0300

    le 5 valores do teclado e printa o menor e o maior

--- org de comp/atvaula.asm
+++ org de comp/atvaula.asm
@@ -1,20 +1,20 @@
 	.data
-str:	.string	"digite 3 valores: "
+str:	.string	"digite 5 valores: "
 strmen:	.string	"menor valor: "
 strmai:	.string	"maior valor: "
 n:	.string	"\n"
 
 	.text
 main:
-	la a0,str
+	la a0,str	
 	li a7,4
-	ecall
+	ecall		#"digite 5 valores: "
 	
 	li a1,0		#contador
-	li a2,4		#condicional de fim de laco
+	li a2,4		#condicional de fim de laco (quantidade de valores -1)
 	
 	li a7,5
-	ecall
+	ecall		#le o primeiro valor
 	add a3,zero,a0		#menor
 	add a4,zero,a0		#maior
 laco:
@@ -42,15 +42,19 @@ maior:
 fim:	
 	li a7,4
 	la a0,strmen
-	ecall
+	ecall		#"menor valor: "
 	mv a0,a3
 	li a7,1
-	ecall
+	ecall		#o menor valor encontrado
+	
+	li a7,4
+	la a0,n
+	ecall		#\n
 	
 	li a7,4
 	la a0,strmai
+	ecall		#"maior valor: "
 	mv a0,a4
 	li a7,1
-	ecall
-	
+	ecall		#o maior valor encontrado
 	
