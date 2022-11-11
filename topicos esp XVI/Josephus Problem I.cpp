6ee770306750e370ce64970de748ba1434262583
kevin willian <kevin_willian@hotmail.com.br>
2022-11-10 22:11:28 -0300

    Update Josephus Problem I.cpp

--- topicos esp XVI/Josephus Problem I.cpp
+++ topicos esp XVI/Josephus Problem I.cpp
@@ -13,22 +13,23 @@ int main()
         vet.push_back(i);
     }
     while(n>0){
-        if(vet[atual] != 300000 && vet[excl]!=300000){
-            res.push_back(vet[excl]);
-            vet[excl]=300000;
-            n--;
-            atual=atual+2;
-            excl=atual+1;
-            if(excl>=vet.size() && atual!=300000){
-                atual=-1;
-                excl=atual+1;
-            }
-        }else if(vet[atual] != 300000){
-            excl++;
-        }
         if(vet[atual] == 300000){
             atual++;
             excl=atual+1;
+        }else{
+            if(vet[atual] != 300000 && vet[excl]!=300000){
+                res.push_back(vet[excl]);
+                vet[excl]=300000;
+                n--;
+                atual=atual+2;
+                excl=atual+1;
+                if(excl>=vet.size() && atual!=300000){
+                    atual=-1;
+                    excl=atual+1;
+                }
+            }else if(vet[atual] != 300000){
+                excl++;
+            }
         }
     }
 }
