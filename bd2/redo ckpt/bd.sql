a623f695d508d4e7974f4e84195f13ab0fd23b54
kevin willian <kevin_willian@hotmail.com.br>
2022-12-04 18:39:38 -0300

    Add files via upload

---
+++ bd2/redo ckpt/bd.sql
@@ -0,0 +1,3 @@
+create database teste;
+create table tredo (id integer, a integer, b integer);
+insert into tredo (id, a, b) values (6,7,8), (7,8,9);
---
+++ bd2/redo ckpt/tredo.py
@@ -0,0 +1,23 @@
+texto = open("entradaLog.txt")	#abre o arquivo para leitura 'r'
+arq = texto.readlines()			#arq vira um vetor com uma linha em cada posição
+'''for linha in arq:			#for percorre todas as posições do vetor
+	#linha=linha.rstrip('\n')	#retira o caractere '\n' da variavel que será printada
+	print(linha)			#printa a linha percorrida atual
+print("----\n"+arq[0])			#printa através da posição do vetor'''
+
+import psycopg2
+conn = psycopg2.connect (	#cria conexao com o BD
+	dbname = "teste",
+	user = "postgres",
+	password = "postgres",
+	host = "localhost",
+	port = "5432")
+cur = conn.cursor()			#conecta ao BD
+
+cur.execute('SELECT * FROM tredo')
+data = cur.fetchall()
+for linha in data:		#lê os da tabela no BD
+	print('id:', linha[0],end=" ")		#o end substitui o ultimo caracter '/n' pelo que for colocado entre as aspas, aqui serve para imprimir tudo na mesma linha
+	print('A:', linha[1],end=" ")
+	print('B:', linha[2])
+	print('-----------------------')
