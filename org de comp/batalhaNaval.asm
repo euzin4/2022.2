43b33b8fa4b67eedcbf5e4dc48f99ff2f2fa2609
kevin willian <kevin_willian@hotmail.com.br>
2023-02-13 17:33:56 -0300

    Update batalhaNaval.asm

--- org de comp/batalhaNaval.asm
+++ org de comp/batalhaNaval.asm
@@ -1,12 +1,14 @@
-		.data
-matriz_navios:	.word	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-tam:		.word	100
+			.data
+matriz_navios:		.word	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+tam:			.word	100
 
-matriz_usuario:	.string	"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-navios:		.string	"3\n1 5 1 1\n0 5 2 2\n0 1 6 4"
-ql:		.string "\n"
-txt_menu:	.string	"1)reiniciar jogo\n2)imprimir matriz_navios\n"
-txt_erro:	.string	"valor inválido\n\n"
+matriz_usuario:		.string	"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+navios:			.string	"3\n1 5 1 1\n0 5 2 2\n0 1 6 4"
+ql:			.string "\n"
+txt_menu:		.string	"1)reiniciar jogo\n2)imprimir matriz_navios\n"
+txt_erro:		.string	"valor inválido\n\n"
+indice_imprime:		.string "  0 1 2 3 4 5 6 7 8 9\n"
+espaco:			.string " "
 
 		.text
 main:
@@ -56,26 +58,50 @@ qbl:
 	li a7,4
 	ecall
 	li t3,0		#zera o cont de ql
-	li a7,1		#volta o a7 para printar int
+	
+	addi t5,t5,1	#soma 1 no contador do indice de linha
+	mv a0,t5
+	li a7,1
+	ecall
+	la a0,espaco
+	li a7,4
+	ecall
+	
 	j voltaql
 	
 imprime_matriz_navios:
+	la a0,indice_imprime
+	li a7,4
+	ecall		#imprime os indices de coluna
+	
 	la t0,matriz_navios
 	li a7,1
 	
 	la t1,tam
-	lw t1,0(t1)	#t1=100
+	lw t1,0(t1)	#t1=100 tamanho da matriz
 	li t2,0		#t2=0 contador
 	li t3,0		#t3=0 contador quebra de linha
-	li t4,10	#t4=10
+	li t4,10	#t4=10 limite para quebra de linha, limite indice de linha
+	li t5,0		#t5=0 contador indice de linha
+	
+	mv a0,t5
+	li a7,1
+	ecall
+	la a0,espaco
+	li a7,4
+	ecall
 loop1:
 	lw a0,0(t0)
+	li a7,1
+	ecall
+	la a0,espaco
+	li a7,4
 	ecall
 	addi t0,t0,4	#+4 endereço
 	addi t2,t2,1	#+1 contador
 	addi t3,t3,1	#+1 contador quebra de linha
 	beq t3,t4,qbl	#se t3 == t4(10)
 voltaql:
-	blt t2,t1,loop1
+	blt t2,t1,loop1	#se contador<100
 
 	ret
